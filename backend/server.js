9e3e703ff97d188fbb9714322b3f99bd060a072a
Simagine/Sadek-Amine <cscience670@gmail.com>
2026-04-06 02:04:38 +0100

    Update server.js
    s

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use("/appointments", require("./routes/appointments"));
 
 app.use("/rooms", require("./routes/rooms"));
 app.use("/invoices", require("./routes/invoices"));
-
+app.use("/auth", require("./routes/auth"));
 
 app.use("/admissions", require("./routes/admissions"));
 app.use("/dashboard", require("./routes/dashboard"));
